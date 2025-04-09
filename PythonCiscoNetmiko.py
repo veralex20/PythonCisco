b38c5e4407cfad43d3054468c0ed3cd71ae08cbf
Alexis <153469896+veralex20@users.noreply.github.com>
2025-04-09 12:35:44 -0400

    Creación de script de ejemplo
    Comando ejecutado: show ip int brief

---
+++ PythonCiscoNetmiko.py
@@ -0,0 +1,21 @@
+from netmiko import ConnectHandler
+
+# Datos del router
+R1 = {
+    'device_type': 'cisco_ios',  # Ajustar según dispositivo
+    'host': '192.168.228.129',     # IP del router en GNS3
+    'username': 'cisco',
+    'password': 'cisco',
+#    'secret': 'admin',           # Si usamos clave para enable
+}
+
+# Conexión al dispositivo
+net_connect = ConnectHandler(**R1)
+#net_connect.enable()  # Entra al modo enable
+
+# Enviar un comando
+output = net_connect.send_command('show ip int brief')
+print(output)
+
+# Cerrar conexión
+net_connect.disconnect()
